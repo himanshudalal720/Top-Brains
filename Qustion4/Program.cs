c60de886918e4c3359d16e8c3c3f8bfd0396cb01
Himanshu <himanshudalal720@gmail.com>
2026-01-20 10:04:48 +0530

    updated files

--- Qustion4/Program.cs
+++ Qustion4/Program.cs
@@ -13,15 +13,23 @@ class StudentComparer : IComparer<Student>
         int marksCompare = y.Marks.CompareTo(x.Marks);
         if (marksCompare != 0)
             return marksCompare;
-
         return x.Age.CompareTo(y.Age);
     }
 }
+
 class Program
 {
     static void Main()
     {
         List<Student> students = new List<Student>
-        
+        {
+            new Student { Name = "Aman", Age = 20, Marks = 90 },
+            new Student { Name = "Riya", Age = 19, Marks = 90 },
+            new Student { Name = "Kunal", Age = 21, Marks = 85 },
+            new Student { Name = "Sneha", Age = 20, Marks = 95 }
+        };
+        students.Sort(new StudentComparer());
+        foreach (var s in students)
+            Console.WriteLine($"{s.Name} {s.Age} {s.Marks}");
     }
 }
